a7f68e52eb1dcd181588d92dfeb799fdf7688cd2
Mohsin Shaikh <mohsin.shaikh324@gmail.com>
2022-09-19 06:19:27 -0700

    make it better

---
+++ preprocessor.py
@@ -0,0 +1,51 @@
+import re
+import pandas as pd
+
+
+def preprocess(data):
+    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s?(?:AM|PM|am|pm)\s-\s'
+
+    messages = re.split(pattern, data)[1:]
+
+    dates = re.findall(pattern, data)
+
+    df = pd.DataFrame({'username': messages, 'date': dates})
+
+    df['date'] = pd.to_datetime(df['date'], format='%m/%d/%y, %I:%M %p - ')
+
+    users = []
+    messages = []
+
+    for message in df['username']:
+        entry = re.split('([\w\W]+?):\s', message)
+        if entry[1:]:
+            users.append(entry[1])
+            messages.append(entry[2])
+        else:
+            users.append('group notifications')
+            messages.append(entry[0])
+
+    df['users'] = users
+    df['messages'] = messages
+    df.drop(columns=['username'], inplace=True)
+
+    df['Year'] = df['date'].dt.year
+    df['Month'] = df['date'].dt.month_name()
+    df['Day'] = df['date'].dt.day
+    df['Hour'] = df['date'].dt.hour
+    df['Minute'] = df['date'].dt.minute
+    df['month_num'] = df['date'].dt.month
+    df['datetimeline'] = df['date'].dt.date
+    df['day_name'] = df['date'].dt.day_name()
+
+    period = []
+    for hour in df[['day_name', 'Hour']]['Hour']:
+        if hour == 23:
+            period.append(str(hour) + "-" + str('00'))
+        elif hour == 0:
+            period.append(str('00') + "-" + str(hour + 1))
+        else:
+            period.append(str(hour) + "-" + str(hour + 1))
+    df['period'] = period
+
+    return df
